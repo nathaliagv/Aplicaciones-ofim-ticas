d24eda9dfb7d37778f99f2e286e269a9835ea7d7
JosePrositoxz <jose.granda@uerdf.edu.ec>
2025-11-16 20:11:07 -0500

    version 2

--- Trabajocongit.py
+++ Trabajocongit.py
@@ -1,16 +1,32 @@
-print("Calculadora simple")
+print("Calculadora con validación")
 
-num1 = float(input("Ingresa el primer número: "))
-operador = input("Ingresa el operador (+, -, *, /): ")
-num2 = float(input("Ingresa el segundo número: "))
+def pedir_numero(mensaje):
+    while True:
+        try:
+            return float(input(mensaje))
+        except ValueError:
+            print(" Error: ingresa un número válido.")
 
-if operador == "+":
-    print("Resultado:", num1 + num2)
-elif operador == "-":
-    print("Resultado:", num1 - num2)
-elif operador == "*":
-    print("Resultado:", num1 * num2)
-elif operador == "/":
-    print("Resultado:", num1 / num2)
+def pedir_operador():
+    operadores = ["+", "-", "*", "/"]
+    while True:
+        op = input("Ingresa el operador (+, -, *, /): ")
+        if op in operadores:
+            return op
+        print(" Error: operador no válido.")
+
+num1 = pedir_numero("Ingresa el primer número: ")
+operador = pedir_operador()
+num2 = pedir_numero("Ingresa el segundo número: ")
+
+if operador == "/" and num2 == 0:
+    print(" Error: no puedes dividir para 0.")
 else:
-    print("Operador no válido")
+    if operador == "+":
+        print("Resultado:", num1 + num2)
+    elif operador == "-":
+        print("Resultado:", num1 - num2)
+    elif operador == "*":
+        print("Resultado:", num1 * num2)
+    elif operador == "/":
+        print("Resultado:", num1 / num2)
